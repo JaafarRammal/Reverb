a8cda6fa075db2fbb5816106f7e97e4003476b62
Harshil <harshils.how.to@gmail.com>
2020-02-14 12:52:33 +0000

    For batch tickets, qr code can be reused to scan group in in parts (for instances where not everyone arrives at the same time)

--- QRScanner/Controller/QRScannerViewController.swift
+++ QRScanner/Controller/QRScannerViewController.swift
@@ -13,8 +13,11 @@ class QRScannerViewController: UIViewController {
     let defaults = UserDefaults.standard
     var orderNums = [String: [String]]()
     var attendees = [String]()
-    var scanned = [String]()
+    var scanned = [String: [String]]()
     
+    @IBOutlet weak var numTicketsScanned: UILabel!
+    @IBOutlet weak var ticketsScannedLabel: UILabel!
+   
     @IBOutlet weak var scannerView: QRScannerView! {
         didSet {
             scannerView.delegate = self
@@ -27,63 +30,61 @@ class QRScannerViewController: UIViewController {
     }
 
 
-    @IBAction func importData(_ sender: Any) {
-
-        let alert = UIAlertController(title: "Restricted action", message: "Please enter admin password", preferredStyle: .alert)
-
-        alert.addTextField { (textField) in
-            textField.text = ""
-            textField.isSecureTextEntry = true
-        }
-
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
-            let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
-            if(textField?.text == "password"){
-
-                let alert = UIAlertController(title: "Sensitive action", message: "Please enter new data", preferredStyle: .alert)
-
-                alert.addTextField { (textField) in
-                    textField.text = ""
-                }
-
-                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
-                    let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
-                    let arr = textField?.text?.components(separatedBy: ",")
-                    self.defaults.set(arr, forKey: "attendees")
-                    self.attendees = self.defaults.stringArray(forKey: "attendees")!
-                    self.showToast(message : "Data updated successfully")
-                }))
-
-                alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { [weak alert] (_) in
-                    self.showToast(message : "Action cancelled")
-                }))
-
-                self.present(alert, animated: true, completion: nil)
-
-            }else{
-                self.showToast(message : "Incorrect password")
-            }
-
-        }))
-
-        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { [weak alert] (_) in
-        }))
-
-        self.present(alert, animated: true, completion: nil)
-    }
+//    @IBAction func importData(_ sender: Any) {
+//
+//        let alert = UIAlertController(title: "Restricted action", message: "Please enter admin password", preferredStyle: .alert)
+//
+//        alert.addTextField { (textField) in
+//            textField.text = ""
+//            textField.isSecureTextEntry = true
+//        }
+//
+//        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+//            let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
+//            if(textField?.text == "password"){
+//
+//                let alert = UIAlertController(title: "Sensitive action", message: "Please enter new data", preferredStyle: .alert)
+//
+//                alert.addTextField { (textField) in
+//                    textField.text = ""
+//                }
+//
+//                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+//                    let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
+//                    let arr = textField?.text?.components(separatedBy: ",")
+//                    self.defaults.set(arr, forKey: "attendees")
+//                    self.attendees = self.defaults.stringArray(forKey: "attendees")!
+//                    self.showToast(message : "Data updated successfully")
+//                }))
+//
+//                alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { [weak alert] (_) in
+//                    self.showToast(message : "Action cancelled")
+//                }))
+//
+//                self.present(alert, animated: true, completion: nil)
+//
+//            }else{
+//                self.showToast(message : "Incorrect password")
+//            }
+//
+//        }))
+//
+//        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { [weak alert] (_) in
+//        }))
+//
+//        self.present(alert, animated: true, completion: nil)
+//    }
 
     var qrData: QRData? = nil {
         didSet {
             if qrData != nil {
 
                 let code = String(qrData!.codeString!.split(separator: "-")[1])
-
+            
                 if isValid(data: code, compare: [String](self.orderNums.keys)){
                     let order = self.orderNums[code]!
-                    if (Int(order[1])! > 1) {
-//                        Have option to select number of tickets being scanned
-                        print("to be done")
-                    }
+                    var quantity = 0
+            
                     let output = order[0] + "\n Quantity: " + order[1] + "\n" + order[2]
                     let alertController = UIAlertController(title: "Valid ticket", message:
                            output, preferredStyle: .alert)
@@ -91,17 +92,74 @@ class QRScannerViewController: UIViewController {
                                action in
                                self.scannerView.startScanning()
                            }))
-//                       remove from list here
-                    self.orderNums.removeValue(forKey: code)
-                    self.scanned.append(code)
-                       self.present(alertController, animated: true, completion: nil)
+
+                    if (Int(order[1])! > 1) {
+    //                        Have option to select number of tickets being scanned
+    //                        Handle cases for too large a quantity entered
+                        var ticketsLeft = ""
+                        if (self.scanned.keys.contains(code)) {
+                            ticketsLeft = String(Int(order[1])! - Int(self.scanned[code]![1])!)
+                        } else {
+                            ticketsLeft = order[1]
+                        }
+                        let titleText = "Customer has " + ticketsLeft + " tickets left, how many would you like to scan in?"
+                        
+                        let ac = UIAlertController(title: titleText, message: nil, preferredStyle: .alert)
+                        ac.addTextField()
+                        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned ac] _ in
+                            
+                            let answer = ac.textFields![0]
+                            quantity = Int(answer.text!)!
+                            
+                            if !(self.scanned.keys.contains(code)) {
+                                var zeroList = self.orderNums[code]
+                                zeroList![1] = "0"
+                                self.scanned[code] = zeroList
+                            }
+
+                            if (quantity == Int(order[1])) {
+                                print("Got all")
+                                self.scanned[code] = self.orderNums[code]
+                                self.orderNums.removeValue(forKey: code)
+                            }
+                            else if ((quantity + Int(self.scanned[code]![1])! <= Int(order[1])!) && (quantity >= 0)) {
+                                var newList = self.scanned[code]
+                                newList![1] = String((Int(newList![1]) ?? 0) + quantity)
+                                self.scanned.updateValue(newList!, forKey: code)
+                                
+                                if (self.scanned[code]![1] == self.orderNums[code]![1]) {
+                                    self.orderNums.removeValue(forKey: code)
+                                }
+//                                print(self.scanned[code]![1])
+                            }
+                            
+                        else {
+                            print("HANDLE CHECK IF THEY TRY TO SCAN TOO MANY TICKETS")
+                            print(self.scanned[code]![1])
+                        }
+                    }
+                        ac.addAction(submitAction)
+//                        print(quantity)
+                        present(ac, animated: true)
+
+                    }
+                    print(quantity)
+
+//                    self.scanned[code] = self.orderNums[code]
+
+                  
+                    self.present(alertController, animated: true, completion: nil)
+                    self.numTicketsScanned.text = String(self.scanned.keys.count)
                 }else{
                     var message_text = "Invalid ticket"
-                    if (self.scanned.contains(code)){
+                    var output = code
+                    if (self.scanned.keys.contains(code)){
                         message_text = "Ticket already scanned"
+                        let order = self.scanned[code]!
+                        output = order[0] + "\n Quantity: " + order[1] + "\n" + order[2]
                     }
                     let alertController = UIAlertController(title: message_text, message:
-                            code, preferredStyle: .alert)
+                            output, preferredStyle: .alert)
                         alertController.addAction(UIAlertAction(title: "Okay", style: .default,handler: {
                                 action in
                                 self.scannerView.startScanning()
@@ -160,11 +218,11 @@ class QRScannerViewController: UIViewController {
                 self.orderNums[pointsArr[7]] = [pointsArr[2], pointsArr[3], pointsArr[4]]
             }
         }
-        print(self.orderNums)
-        print(self.orderNums.count)
-        
+//        print(self.orderNums)
+//        print(self.orderNums.count)
+        self.numTicketsScanned.text = "0"
     }
-    
+        
     
     func csv(data: String) -> [[String]] {
         var result: [[String]] = []
